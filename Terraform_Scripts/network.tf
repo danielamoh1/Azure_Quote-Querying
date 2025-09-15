2e8ab0b972a65dfafefc7fccc34a4708b3ce77e3
danielamoh1 <160555417+danielamoh1@users.noreply.github.com>
2025-09-14 23:51:15 -0400

    Update network.tf

--- Terraform_Scripts/network.tf
+++ Terraform_Scripts/network.tf
@@ -1,8 +1,8 @@
 resource "azurerm_virtual_network" "vnet" {
-  name                = "quotes-vnet-tf"
-  address_space       = ["11.0.0.0/16"]
+  name                = "quote-vnet-tf"
   location            = azurerm_resource_group.app.location
   resource_group_name = azurerm_resource_group.app.name
+  address_space       = ["11.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "db_subnet" {
@@ -10,8 +10,7 @@ resource "azurerm_subnet" "db_subnet" {
   resource_group_name  = azurerm_resource_group.app.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["11.0.1.0/24"]
-
-  enforce_private_link_endpoint_network_policies = true
+  private_endpoint_network_policies_enabled = true
 }
 
 resource "azurerm_subnet" "webapp_subnet" {
