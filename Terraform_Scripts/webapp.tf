f22aee854c7ee80778d1cb0294c9953021b9d2a4
danielamoh1 <160555417+danielamoh1@users.noreply.github.com>
2025-09-14 23:50:01 -0400

    Update webapp.tf

--- Terraform_Scripts/webapp.tf
+++ Terraform_Scripts/webapp.tf
@@ -1,30 +1,41 @@
-# App Service Plan
-resource "azurerm_service_plan" "appserviceplan" {
-  name                = "quotes-asp-tf"
+resource "azurerm_service_plan" "plan" {
+  name                = "quotes-serviceplan-tf"
   location            = azurerm_resource_group.app.location
   resource_group_name = azurerm_resource_group.app.name
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "P1v3"
+  zone_balancing_enabled = true
 }
 
-# Web App
 resource "azurerm_linux_web_app" "webapp" {
   name                = "quotes-webapp-tf"
   location            = azurerm_resource_group.app.location
   resource_group_name = azurerm_resource_group.app.name
-  service_plan_id     = azurerm_service_plan.appserviceplan.id
+  service_plan_id     = azurerm_service_plan.plan.id
 
   site_config {
     application_stack {
       node_version = "20-lts"
     }
+    vnet_route_all_enabled = true
   }
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = "1"
-    "DB_USER"                  = "quotes-sql-admin"
-    "DB_PASSWORD"              = random_password.sql.result
-    "DB_SERVER"                = azurerm_mssql_server.sql.fully_qualified_domain_name
-    "DB_NAME"                  = azurerm_mssql_database.db.name
+    "ConnectionStrings__QuotesDb" = "Server=tcp:${azurerm_mssql_server.sql.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db.name};Persist Security Info=False;User ID=${azurerm_mssql_server.sql.administrator_login};Password=${random_password.sql.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;"
+  }
+}
+
+resource "azurerm_private_endpoint" "sql_pe" {
+  name                = "quotes-sql-pe-tf"
+  location            = azurerm_resource_group.app.location
+  resource_group_name = azurerm_resource_group.app.name
+  subnet_id           = azurerm_subnet.db_subnet.id
+
+  private_service_connection {
+    name                           = "quotes-sql-privateservice-tf"
+    private_connection_resource_id = azurerm_mssql_server.sql.id
+    subresource_names              = ["sqlServer"]
+    is_manual_connection           = false
   }
 }
