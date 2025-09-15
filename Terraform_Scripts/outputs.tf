55184807c3bc4742d1e6845da52df546a74cef21
danielamoh1 <160555417+danielamoh1@users.noreply.github.com>
2025-09-14 23:49:31 -0400

    Update outputs.tf

--- Terraform_Scripts/outputs.tf
+++ Terraform_Scripts/outputs.tf
@@ -1,5 +1,5 @@
 output "sql_admin_username" {
-  value = "quotes-sql-admin"
+  value = azurerm_mssql_server.sql.administrator_login
 }
 
 output "sql_admin_password" {
@@ -7,7 +7,6 @@ output "sql_admin_password" {
   sensitive = true
 }
 
-output "connection_string" {
-  value = "Server=tcp:${azurerm_mssql_server.sql.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db.name};Persist Security Info=False;User ID=quotes-sql-admin;Password=${random_password.sql.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;"
-  sensitive = true
+output "private_endpoint_name" {
+  value = azurerm_private_endpoint.sql_pe.name
 }
